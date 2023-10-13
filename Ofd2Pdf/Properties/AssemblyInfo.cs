3194bcfac1cfa07ed2f15df97fda2b9bdf8d54d5
taurusxin <i@taurusxin.com>
2023-10-13 20:38:01 +0800

    release: 1.2.0.0

--- Ofd2Pdf/Properties/AssemblyInfo.cs
+++ Ofd2Pdf/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Ofd2Pdf")]
 [assembly: AssemblyDescription("Convert OFD File to PDF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hangzhou Rhyland Technology Co., Ltd.")]
+[assembly: AssemblyCompany("Hangzhou Ruilan Technology Co., Ltd.")]
 [assembly: AssemblyProduct("Ofd2Pdf")]
 [assembly: AssemblyCopyright("Copyright © Rhyland 2022")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
